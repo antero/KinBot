23bf0864e13f773f318c098d5ff2d1bc33753457
Vitor Antero <vitor.melo@gmail.com>
2013-02-08 11:26:12 -0300

    configuracao kinect

--- KinBot/source/main.cpp
+++ KinBot/source/main.cpp
@@ -7,3 +7,13 @@ This projects needs the following libraries:
 # OpenCV v2.4.3 x86
 # Kinect SDK v1.6 x86
 */
+//#include <NuiApi.h>
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+
+int main() {
+	//INuiSensor* sensor;
+	return 0;
+}
